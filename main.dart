f5f378d22aed592cee4aa52dd11058224fd04308
nsoroosvelt <ntsoutebah2020@gmail.com>
2022-09-16 16:32:34 +0100

    second commit

--- main.dart
+++ main.dart
@@ -1,3 +1,47 @@
-void main() {
-  print("Hello World");
+import 'dart:io';
+import 'dart:math' show sqrt;
+
+int readInt() {
+  //in Dart 3 there is a feature Added Called Null Safety which
+  // is used to guarantee that the input won't Be Null So you need
+  // just add a '!' in Your 'stdin.readLineSync()' and You are done Here is The Code 
+  String input = stdin.readLineSync()!;
+  return int.parse(input);
+}
+
+main() {
+
+  print("A quadratic equation is something of the form `a⋅x² + b⋅x + c = 0`.");
+  print("input 3 numbers, pressing enter after each, signifying your coefficient values for a, b and c respectively");
+  int A, B, C;
+  try {
+    A = readInt();
+    B = readInt();
+    C = readInt();
+  }
+  on FormatException {
+    print("Coefficient is not a number.");
+    return;
+  }
+  if (A == 0) {
+    print("Not a quadratic equation.");
+    return;
+  }
+
+  print("your equation is: ${A}x² + ${B}x + ${C} = 0");
+  int D = B * B - 4 * A * C;
+  double p1 = - B / 2.0 / A;
+  double p2 = sqrt(D.abs()) / 2.0 / A;
+  print("Its roots are:");
+  if (D == 0) {
+    print("x = $p1");
+  } else {
+    if (D > 0) {
+      print("x1 = ${p1 + p2}");
+      print("x2 = ${p1 - p2}");
+    } else {
+      print("x1 = ($p1, $p2)");
+      print("x2 = ($p1, ${-p2})");
+    }
+  }
 }
